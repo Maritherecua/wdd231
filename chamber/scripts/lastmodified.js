a035cf32d1a3d18f617014180ed565ef991851df
Maritherecua <mrtrsarroyo@yahoo.com>
2026-01-17 13:38:43 -0800

    current year

--- chamber/scripts/lastmodified.js
+++ chamber/scripts/lastmodified.js
@@ -1,3 +1,3 @@
-const dateObject = new Date;
-const getCurrentYEar = document.querySelector("#currentYear").textContent += dateObject.getFullYear();
-document.querySelector("#lastModified").textContent = `Last Modified ${new Date(document.lastModified).toLocaleDateString()}`
+const dateObject = new Date();
+document.querySelector("#currentYear").textContent = dateObject.getFullYear();
+document.querySelector("#lastModified").textContent = `Last Modified: ${new Date(document.lastModified).toLocaleDateString()}`
