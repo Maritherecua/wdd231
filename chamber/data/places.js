8b57171ef413c58c15c7f400cd965bc67fed60f6
Maritherecua <mrtrsarroyo@yahoo.com>
2026-02-05 16:49:13 -0800

    places js file

---
+++ chamber/data/places.js
@@ -0,0 +1,53 @@
+export const places = [
+    {
+        "name": "The Promenade Shops at Orchard Valley",
+        "description": "A great place to spend a day shopping and dining with family and friends.",
+        "address": "3110 Promenade Pkwy, Manteca, CA 95336",
+        "image_link": "promenade.webp"
+    },
+    {
+        "name": "Bass Pro Shops",
+        "description": "A popular outdoor retail store that offers a wide range of products for fishing, hunting, camping, and more.",
+        "address": "100 Bass Pro Dr, Manteca, CA 95336",
+        "image_link": "basspro.webp"
+    },
+    {
+        "name": "Manteca Cinema",
+        "description": "A local movie theater that shows the latest films and offers a fun and affordable entertainment option for families and friends.",
+        "address": "1001 W Yosemite Ave, Manteca, CA 95337",
+        "image_link": "cinema.webp"
+    },
+    {
+        "name": "Manteca Skate Park",
+        "description": "A popular skate park that offers a fun and challenging environment for skateboarders of all skill levels.",
+        "address": "1001 W Yosemite Ave, Manteca, CA 95337",
+        "image_link": "skatepark.webp"
+    },
+    {
+        "name": "Manteca Golf Course",
+        "description": "A beautiful golf course that offers a challenging and enjoyable experience for golfers of all skill levels.",
+        "address": "500 E North St, Manteca, CA 95336",
+        "image_link": "golfcourse.webp"
+    },
+    {
+        "name": "Yosemite Valley Discovery Walk",
+        "description": "A world-famous national park known for its stunning natural beauty, including towering granite cliffs, waterfalls, and diverse wildlife. It's a popular destination for hiking, rock climbing, and sightseeing."
+        , "address": "Yosemite National Park, CA 95389",
+        "image_link": "yosemite.webp",
+        "cost": "$35 per vehicle (valid for 7 days)"
+    },
+    {
+        "name": "Giant Sequoias Day Hike",
+        "description": "A day hike that takes you through a grove of giant sequoia trees, some of the largest and oldest living organisms on Earth. The hike is moderate in difficulty and offers stunning views of the trees and surrounding landscape."
+        , "address": "Mariposa Grove, Yosemite National Park, CA 95389",
+        "image_link": "sequoias.webp",
+        "cost": "$35 per vehicle (valid for 7 days)"
+    },
+    {
+        "name": "Glacier Point",
+        "description": "A viewpoint that offers breathtaking panoramic views of Yosemite Valley, Half Dome, and the surrounding mountains. It's a popular spot for sunrise and sunset photography."
+        , "address": "Glacier Point Rd, Yosemite National Park, CA 95389",
+        "image_link": "glacierpoint.webp",
+        "cost": "$35 per vehicle (valid for 7 days)"
+    }
+]
