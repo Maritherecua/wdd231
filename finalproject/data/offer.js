9d52ba4682a93da224935328056efea03911a5a6
Maritherecua <mrtrsarroyo@yahoo.com>
2026-02-09 20:05:48 -0800

    js file

---
+++ finalproject/data/offer.js
@@ -0,0 +1,41 @@
+export const offers = [
+    {
+        "offer": 1,
+        "title": "Guided meditation",
+        "details": ["A relaxing guided meditation session to help you unwind and de-stress."]
+    },
+    {
+        "offer": 2,
+        "title": "Yoga class",
+        "details": ["A beginner-friendly yoga class to improve flexibility, balance, strength, and mental clarity."]
+    },
+    {
+        "offer": 3,
+        "title": "Stress and Pain Management Workshop",
+        "details": ["A workshop focused on techniques to manage stress and alleviate pain through various methods such as relaxation exercises, mindfulness, and physical therapy."]
+    },
+    {
+        "offer": 4,
+        "title": "Mindfulness workshop",
+        "details": ["A workshop focused on developing mindfulness techniques to reduce stress and improve overall well-being."]        
+    },
+    {
+        "offer": 5,
+        "title": "Art therapy session",
+        "details": ["A creative art therapy session designed to help participants express their emotions and reduce stress through artistic activities."]
+    },
+    {
+        "offer": 6,
+        "title": "Coaching to let go of unhealthy habits",
+        "details": ["A set of coaching sessions aimed at helping participants identify and release unhealthy habits to improve overall well-being."]
+    },
+    {
+        "offer": 7,
+        "title": "Hypnotherapy sessions",
+        "details": ["Interactive hypnotherapy sessions designed to promote relaxation and positive behavioral changes through guided hypnosis techniques."]
+    },
+    {
+        "offer": 8,
+        "title": "Stress relief techniques",
+        "details": ["A session focused on teaching various stress relief techniques such as deep breathing, progressive muscle relaxation, and visualization exercises."]
+    }]
